7698f068b83fd0d12c538e19ec11cd490304c3ab
haqt115 <144076557+haqt115@users.noreply.github.com>
2025-05-21 15:40:59 +0700

    Update scraper_factory.py

--- VietNewsSummerize-crawl_17-05/Crawler/Sample/Scraper/scraper_factory.py
+++ VietNewsSummerize-crawl_17-05/Crawler/Sample/Scraper/scraper_factory.py
@@ -1,11 +1,7 @@
 from vnexpress_scraper import VNExpressScraper
-from cafef_scraper import CafeFScraper
-from tienphong_scraper import TienPhongScraper
 
-def get_scraper(name, headle, base_url, output_csv, process, persistent_file, emb_csv):
+def get_scraper(name, headle, base_url, output_csv, process):
     if name == "VNExpress":
-        return VNExpressScraper(headle, base_url, output_csv, process, persistent_file, emb_csv)
-    elif name == "TienPhong":
-        return TienPhongScraper(headle, base_url, output_csv, process, persistent_file, emb_csv)
+        return VNExpressScraper(headle, base_url, output_csv, process)
     else:
         raise ValueError(f"Unknown scraper: {name}")
